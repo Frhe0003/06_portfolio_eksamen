584f85e98539602889ef58a53f2e0e297f0802bf
Freja <frhe0003@stud.kea.dk>
2024-06-11 15:22:31 +0200

    ændring

--- menu.js
+++ menu.js
@@ -1,16 +1,20 @@
-const burger = document.querySelector(".burger");
-const nav = document.querySelector("nav");
-const menu = document.querySelector(".menu");
-const links = document.querySelectorAll(".menu li a");
+// opretter konstanter
+const burger = document.querySelector(".burger"); // burger ikonet
+const nav = document.querySelector("nav"); // navigationselement
+const menu = document.querySelector(".menu"); // Menu-listen
+const links = document.querySelectorAll(".menu li a"); //Alle links i menuen
 
-burger.addEventListener("click", () => {
-  burger.classList.toggle("active");
-  nav.classList.toggle("active");
+// tilføj en event listener (click) til burger-ikonet for at aktivere burgermenuen. Lukker og åbner menuen
+burger.addEventListener("click", () => { 
+  burger.classList.toggle("active"); //tilføjer eller fjerner "active" klassen fra burger ikonet
+  nav.classList.toggle("active"); //tilføjer eller fjerner "active" klassen fra navigationen (links)
 });
+//når brugeren klikker på bruger ikonet, åbnes eller lukkes menuen
 
+//Tilføj eventlisteners til vert link i menuen. 
 links.forEach((link) => {
   link.addEventListener("click", () => {
-    burger.classList.remove("active");
-    nav.classList.remove("active");
+    burger.classList.remove("active"); //fjerner "active" klassen fra burger ikonet ved klik på et link
+    nav.classList.remove("active"); //fjerner "sctive" klassen fra navigationen ved at klikke på et link
   });
 });
